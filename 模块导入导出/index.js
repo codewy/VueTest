e91e5daa996bb8cbc4aa3e916d6b6f5449dc355c
codewy <sexdnf@163.com>
2020-02-17 17:04:59 +0800

    '第一次提交'

---
+++ 模块导入导出/index.js
@@ -0,0 +1,11 @@
+// 1.导入全部变量
+// import * as aaa from './test.js'
+
+// 导入default自定义名变量
+// import name from './test.js'
+
+//导入多个变量
+import {name,age} from "./test.js"
+
+console.log(name)
+console.log(age)
---
+++ 模块导入导出/test.js
@@ -0,0 +1,21 @@
+const name = '小明'
+const age = 18
+const sex = '男'
+
+function sum(a,b) {
+  return console.log(a+b)
+}
+
+// export {
+//   name,
+//   age,
+//   sex,
+//   sum
+// }
+
+// export default name
+
+export {
+  name,
+  age
+}
